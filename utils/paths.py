2b8fa419b9564a6103c766fd574d9d1f457bd484
Kaden <93230323+kadenee@users.noreply.github.com>
2023-08-20 12:24:05 -0500

    Update paths.py

--- utils/paths.py
+++ utils/paths.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Paths:
     """Manages and configures the paths used by WaveRNN, Tacotron, and the data."""
     def __init__(self, data_path, voc_id, tts_id):
-        self.base = Path(__file__).parent.parent.expanduser().resolve()
+        self.base = Path('/content/drive/MyDrive/ForwardTacotron/')
 
         # Data Paths
         self.data = Path(data_path).expanduser().resolve()
